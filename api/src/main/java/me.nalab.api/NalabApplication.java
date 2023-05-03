91b37c0c4968a6c66446ee0310030ba2161d0ed4
xb205 <62425964+devxb@users.noreply.github.com>
2023-05-03 22:32:03 +0900

    [Set up] : Jacoco task 생성 (#6)
    Co-authored-by: devxb <devxb@naver.com>

---
+++ api/src/main/java/me.nalab.api/NalabApplication.java
@@ -0,0 +1,13 @@
+package me.nalab.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NalabApplication {
+
+	public static void main(String... args){
+		SpringApplication.run(NalabApplication.class, args);
+	}
+
+}
