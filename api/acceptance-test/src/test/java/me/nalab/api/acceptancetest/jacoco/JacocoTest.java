277574699f27ef74a7b27eb177c96ba8357f84eb
devxb <devxb@naver.com>
2023-05-03 22:19:36 +0900

    jacoco setup

---
+++ api/acceptance-test/src/test/java/me/nalab/api/acceptancetest/jacoco/JacocoTest.java
@@ -0,0 +1,13 @@
+package me.nalab.api.acceptancetest.jacoco;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class JacocoTest {
+
+	@Test
+	void JACOCO_GRADLE_SET_UP(){
+		assertEquals(1, 1);
+	}
+
+}
