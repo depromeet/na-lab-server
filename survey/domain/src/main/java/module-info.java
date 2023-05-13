6622e9aadc3c1e5f6e5c4b97eef8ed4ad02099ea
devxb <devxb@naver.com>
2023-05-13 22:48:45 +0900

    [fix] : feedback 패키지 삭제 및 module-info 수정

--- survey/domain/src/main/java/module-info.java
+++ survey/domain/src/main/java/module-info.java
@@ -1,9 +1,7 @@
 module luffy.survey.domain.main {
 
-	exports me.nalab.survey.domain.feedback;
 	exports me.nalab.survey.domain.survey;
 	exports me.nalab.survey.domain.survey.api.feedback;
-	exports me.nalab.survey.domain.survey.api.target;
 	exports me.nalab.survey.domain.target;
 
 	requires lombok;
