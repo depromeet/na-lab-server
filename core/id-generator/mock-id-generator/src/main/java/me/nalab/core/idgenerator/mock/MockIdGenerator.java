8642b1f540d9afef208edecc35d86d39f3b0fd3c
devxb <devxb@naver.com>
2023-05-13 20:59:10 +0900

    [fix] : RANDOM -> to SECURE_RANDOM

--- core/id-generator/mock-id-generator/src/main/java/me/nalab/core/idgenerator/mock/MockIdGenerator.java
+++ core/id-generator/mock-id-generator/src/main/java/me/nalab/core/idgenerator/mock/MockIdGenerator.java
@@ -1,16 +1,17 @@
 package me.nalab.core.idgenerator.mock;
 
+import java.security.SecureRandom;
 import java.util.Random;
 
 import me.nalab.core.idgenerator.idcore.IdGenerator;
 
 public class MockIdGenerator implements IdGenerator {
 
-	private static final Random RANDOM = new Random();
+	private static final SecureRandom SECURE_RANDOM = new SecureRandom();
 
 	@Override
 	public long generate() {
-		return RANDOM.nextLong();
+		return SECURE_RANDOM.nextLong();
 	}
 
 }
