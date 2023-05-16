43caa1b4ab6f1fe652af17e4185a86465886ad83
devxb <devxb@naver.com>
2023-05-16 10:32:45 +0900

    [fix] : delete unused dependency

--- survey/domain/src/main/java/module-info.java
+++ survey/domain/src/main/java/module-info.java
@@ -3,8 +3,6 @@ module luffy.survey.domain.main {
 	exports me.nalab.survey.domain.survey;
 	exports me.nalab.survey.domain.target;
 
-	requires luffy.core.id.generator.id.generator.starter.main;
-
 	requires lombok;
 
 }
