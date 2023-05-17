768138777d813e64f1fedb65b75d12f65470b7ee
수지니 <71487608+ssssujini99@users.noreply.github.com>
2023-05-18 03:49:44 +0900

    [chore] : web-adaptor 모듈 추가 및 기타 의존성 관계 설정

---
+++ survey/web-adaptor/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module luffy.survey.web.adaptor.main {
+
+	requires luffy.survey.application.main;
+
+	requires lombok;
+	requires spring.boot;
+	requires spring.boot.autoconfigure;
+	requires spring.context;
+	requires spring.boot.starter;
+	requires spring.boot.starter.web;
+	requires spring.web;
+	requires com.fasterxml.jackson.annotation;
+
+}
