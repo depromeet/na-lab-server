dc01dc42af630685f67e2a9d51c601e876edcf98
devxb <devxb@naver.com>
2023-05-17 23:56:23 +0900

    [fix] : 생성자 -> lombok으로 수정

--- survey/application/src/main/java/me/nalab/survey/application/service/create/CreateSurveyService.java
+++ survey/application/src/main/java/me/nalab/survey/application/service/create/CreateSurveyService.java
@@ -2,6 +2,7 @@ package me.nalab.survey.application.service.create;
 
 import org.springframework.stereotype.Service;
 
+import lombok.AllArgsConstructor;
 import me.nalab.core.idgenerator.idcore.IdGenerator;
 import me.nalab.survey.application.common.dto.SurveyDto;
 import me.nalab.survey.application.common.mapper.SurveyDtoMapper;
@@ -12,17 +13,12 @@ import me.nalab.survey.application.port.out.persistence.FindTargetPort;
 import me.nalab.survey.domain.survey.Survey;
 
 @Service
+@AllArgsConstructor
 class CreateSurveyService implements CreateSurveyUseCase {
 
 	private final CreateSurveyPort createSurveyPort;
 	private final FindTargetPort findTargetPort;
 	private final IdGenerator idGenerator;
-
-	CreateSurveyService(CreateSurveyPort createSurveyPort, FindTargetPort findTargetPort, IdGenerator idGenerator) {
-		this.createSurveyPort = createSurveyPort;
-		this.findTargetPort = findTargetPort;
-		this.idGenerator = idGenerator;
-	}
 
 	@Override
 	public void createSurvey(Long targetId, SurveyDto surveyDto) {
