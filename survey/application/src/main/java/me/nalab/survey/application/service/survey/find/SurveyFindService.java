2c2085bba7591add44933d4f1823be7fcea08926
수지니 <71487608+ssssujini99@users.noreply.github.com>
2023-05-18 21:16:52 +0900

    [feat] : 유즈케이스 구현체 SurveyFindService 추가

---
+++ survey/application/src/main/java/me/nalab/survey/application/service/survey/find/SurveyFindService.java
@@ -0,0 +1,19 @@
+package me.nalab.survey.application.service.survey.find;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import me.nalab.survey.application.common.dto.SurveyDto;
+import me.nalab.survey.application.port.in.web.SurveyFindUseCase;
+import me.nalab.survey.application.port.out.persistence.survey.find.SurveyFindPort;
+
+@Service
+@RequiredArgsConstructor
+public class SurveyFindService implements SurveyFindUseCase {
+
+	private final SurveyFindPort surveyFindPort;
+	@Override
+	public SurveyDto findSurvey(Long surveyId) {
+		return surveyFindPort.findSurvey(surveyId);
+	}
+}
