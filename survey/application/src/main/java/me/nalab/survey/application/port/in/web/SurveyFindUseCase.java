a3ca74f9223866350398fb1d4f22b290f3f4d885
수지니 <71487608+ssssujini99@users.noreply.github.com>
2023-05-18 21:14:34 +0900

    [feat] : SurveyFindUseCase, TargetFindUseCase 추가

--- survey/application/src/main/java/me/nalab/survey/application/port/in/web/SurveyFindUseCase.java
+++ survey/application/src/main/java/me/nalab/survey/application/port/in/web/SurveyFindUseCase.java
@@ -3,6 +3,6 @@ package me.nalab.survey.application.port.in.web;
 import me.nalab.survey.application.common.dto.SurveyDto;
 
 public interface SurveyFindUseCase {
-
 	SurveyDto findSurvey(Long surveyId);
+
 }
--- survey/application/src/main/java/me/nalab/survey/application/port/in/web/TargetFindUseCase.java
+++ survey/application/src/main/java/me/nalab/survey/application/port/in/web/TargetFindUseCase.java
@@ -3,5 +3,5 @@ package me.nalab.survey.application.port.in.web;
 import me.nalab.survey.application.common.dto.TargetDto;
 
 public interface TargetFindUseCase {
-	TargetDto findTarget(Long surveyId);
+	TargetDto findTarget(Long targetId);
 }
