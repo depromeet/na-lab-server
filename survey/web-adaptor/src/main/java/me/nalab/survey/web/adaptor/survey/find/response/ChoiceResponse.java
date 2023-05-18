8aba66396a1a413ff250f6717593ce7fc059db11
수지니 <71487608+ssssujini99@users.noreply.github.com>
2023-05-18 16:15:31 +0900

    [feat] : 질문 폼 조회 usecase의 web-adaptor 부분 구현 완료 (#40)

---
+++ survey/web-adaptor/src/main/java/me/nalab/survey/web/adaptor/survey/find/response/ChoiceResponse.java
@@ -0,0 +1,23 @@
+package me.nalab.survey.web.adaptor.survey.find.response;
+
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@Builder
+@Getter
+@ToString
+@EqualsAndHashCode
+public class ChoiceResponse {
+
+	@JsonProperty("choice_id")
+	private Long choiceId;
+
+	private String content;
+
+	private Integer order;
+
+}
---
+++ survey/web-adaptor/src/main/java/me/nalab/survey/web/adaptor/survey/find/response/ShortFormQuestionResponse.java
@@ -0,0 +1,14 @@
+package me.nalab.survey.web.adaptor.survey.find.response;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@SuperBuilder
+@Getter
+@ToString
+@EqualsAndHashCode(callSuper = true)
+public class ShortFormQuestionResponse extends FormQuestionResponseable {
+
+}
