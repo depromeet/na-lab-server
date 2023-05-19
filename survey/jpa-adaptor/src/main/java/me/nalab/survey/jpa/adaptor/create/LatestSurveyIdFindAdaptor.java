286107d01655081b5163a2206f4a28a60b46c1e2
devxb <dlwnsdud205@naver.com>
2023-05-19 21:14:16 +0900

    [fix] : EntityManager 주입을 Autowired -> PersistenceContext 로 변경

--- survey/jpa-adaptor/src/main/java/me/nalab/survey/jpa/adaptor/create/LatestSurveyIdFindAdaptor.java
+++ survey/jpa-adaptor/src/main/java/me/nalab/survey/jpa/adaptor/create/LatestSurveyIdFindAdaptor.java
@@ -4,17 +4,17 @@ import java.util.Optional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Repository;
 
-import lombok.RequiredArgsConstructor;
 import me.nalab.survey.application.port.out.persistence.LatestSurveyIdFindPort;
 
 @Repository
-@RequiredArgsConstructor
 public class LatestSurveyIdFindAdaptor implements LatestSurveyIdFindPort {
 
-	private final EntityManager entityManager;
+	@PersistenceContext
+	private EntityManager entityManager;
 
 	@Override
 	public Optional<Long> findLatestSurveyIdByTargetId(Long targetId) {
