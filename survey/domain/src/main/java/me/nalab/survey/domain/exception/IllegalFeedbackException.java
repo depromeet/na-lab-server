2849a2c9050888726221f7455d90bee4edf50bb8
devxb <dlwnsdud205@naver.com>
2023-05-23 00:56:33 +0900

    [refactor] : 예외에서 제네릭 사용하도록 수정

--- survey/domain/src/main/java/me/nalab/survey/domain/exception/IllegalFeedbackException.java
+++ survey/domain/src/main/java/me/nalab/survey/domain/exception/IllegalFeedbackException.java
@@ -1,10 +1,11 @@
 package me.nalab.survey.domain.exception;
 
 import me.nalab.survey.domain.survey.spi.FeedbackValidable;
+import me.nalab.survey.domain.survey.spi.QuestionFeedbackValidable;
 
 public final class IllegalFeedbackException extends RuntimeException {
 
-	public IllegalFeedbackException(FeedbackValidable feedbackable) {
+	public <T extends QuestionFeedbackValidable> IllegalFeedbackException(FeedbackValidable<T> feedbackable) {
 		super("Illegal feedback \"" + feedbackable + "\"");
 	}
 
