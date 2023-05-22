5ef7bfc7aa10ffabdcb62c40b094382b4a4336b1
xb205 <62425964+devxb@users.noreply.github.com>
2023-05-22 11:11:41 +0900

    [feat] : `javax.validation` 전역 예외 핸들러 추가 (#68)

---
+++ core/exception-handler/src/test/java/me/nalab/core/exception/handler/TestRequest.java
@@ -0,0 +1,24 @@
+package me.nalab.core.exception.handler;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+class TestRequest {
+
+	@Length(max = 5, message = "overflow")
+	@NotNull(message = "null")
+	@NotBlank(message = "blank")
+	private String hello;
+
+}
