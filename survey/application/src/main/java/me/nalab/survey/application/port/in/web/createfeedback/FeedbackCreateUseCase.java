d0c51fdd5e402751f7ef7eea8fc83cafb85036ef
devxb <dlwnsdud205@naver.com>
2023-05-23 19:26:26 +0900

    [docs] : 누락된 자바독 추가

--- survey/application/src/main/java/me/nalab/survey/application/port/in/web/createfeedback/FeedbackCreateUseCase.java
+++ survey/application/src/main/java/me/nalab/survey/application/port/in/web/createfeedback/FeedbackCreateUseCase.java
@@ -2,8 +2,17 @@ package me.nalab.survey.application.port.in.web.createfeedback;
 
 import me.nalab.survey.application.common.feedback.dto.FeedbackDto;
 
+/**
+ * 새로운 피드백을 생성하는 인터페이스 입니다.
+ */
 public interface FeedbackCreateUseCase {
 
+	/**
+	 * surveyId와 feedbackDto를 인자로 받고, surveyId에 해당하는 survey에 feedback을 남깁니다.
+	 *
+	 * @param surveyId feedback을 남길 survey의 id
+	 * @param feedbackDto feedback의 정보들
+	 */
 	void createFeedback(Long surveyId, FeedbackDto feedbackDto);
 
 }
