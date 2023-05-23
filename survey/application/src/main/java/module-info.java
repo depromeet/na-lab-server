8974285849f78687debd5fc1fb55a752ec0edbc2
수지니 <71487608+ssssujini99@users.noreply.github.com>
2023-05-24 03:05:47 +0900

    [chore] : 의존성 추가

--- survey/application/src/main/java/module-info.java
+++ survey/application/src/main/java/module-info.java
@@ -10,6 +10,7 @@ module luffy.survey.application.main {
 	exports me.nalab.survey.application.port.in.web.getid;
 	exports me.nalab.survey.application.exception;
 	exports me.nalab.survey.application.port.out.persistence.findid;
+	exports me.nalab.survey.application.port.out.persistence.feedbacksummary;
 
 	requires luffy.survey.domain.main;
 	requires luffy.core.id.generator.id.generator.starter.main;
