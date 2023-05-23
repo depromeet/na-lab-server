55d9195e829ee35456cf9c70a91f877324747af2
수지니 <71487608+ssssujini99@users.noreply.github.com>
2023-05-23 16:15:39 +0900

    [test] : 질문 폼 조회 인수테스트

--- api/acceptance-test/src/test/java/me/nalab/luffy/api/acceptance/test/survey/SurveyAcceptanceValidator.java
+++ api/acceptance-test/src/test/java/me/nalab/luffy/api/acceptance/test/survey/SurveyAcceptanceValidator.java
@@ -33,4 +33,40 @@ public class SurveyAcceptanceValidator {
 		);
 	}
 
+	public static void assertIsSurveyAndQuestionFound(ResultActions resultActions) throws Exception {
+		resultActions.andExpect(status().isOk())
+			.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+			.andExpect(jsonPath("$.target.nickname").value("sujin"))
+			.andExpect(jsonPath("$.question_count").value(4))
+
+			.andExpect(jsonPath("$.question[0].type").value("choice"))
+			.andExpect(jsonPath("$.question[0].title").value("당신의 포지션을 알려주세요."))
+			.andExpect(jsonPath("$.question[0].order").value(3))
+			.andExpect(jsonPath("$.question[0].choices[0].order").value(1))
+			.andExpect(jsonPath("$.question[0].choices[1].order").value(2))
+			.andExpect(jsonPath("$.question[0].choices[2].order").value(3))
+			.andExpect(jsonPath("$.question[0].choices[3].order").value(4))
+
+
+			.andExpect(jsonPath("$.question[1].type").value("choice"))
+			.andExpect(jsonPath("$.question[1].title").value("예진님의 성향은 어떤 것이 돋보이나요?"))
+			.andExpect(jsonPath("$.question[1].order").value(4))
+			.andExpect(jsonPath("$.question[1].choices[0].order").value(1))
+			.andExpect(jsonPath("$.question[1].choices[1].order").value(2))
+			.andExpect(jsonPath("$.question[1].choices[2].order").value(3))
+			.andExpect(jsonPath("$.question[1].choices[3].order").value(4))
+			.andExpect(jsonPath("$.question[1].choices[4].order").value(5))
+			.andExpect(jsonPath("$.question[1].choices[5].order").value(6))
+			.andExpect(jsonPath("$.question[1].choices[6].order").value(7))
+			.andExpect(jsonPath("$.question[1].choices[7].order").value(8))
+
+			.andExpect(jsonPath("$.question[2].type").value("short"))
+			.andExpect(jsonPath("$.question[2].title").value("당신이 생각하는 예진님의 직무적 강점은 무엇인가요?"))
+			.andExpect(jsonPath("$.question[2].order").value(5))
+
+			.andExpect(jsonPath("$.question[3].type").value("short"))
+			.andExpect(jsonPath("$.question[3].title").value("당신이 생각하는 예진님의 직무적 약점은 무엇인가요?"))
+			.andExpect(jsonPath("$.question[3].order").value(6));
+	}
+
 }
