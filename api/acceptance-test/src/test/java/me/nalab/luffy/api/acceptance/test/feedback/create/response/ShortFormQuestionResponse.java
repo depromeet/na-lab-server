d19e375caa3f4c2da5e977b21afcf01042b86ab8
xb205 <62425964+devxb@users.noreply.github.com>
2023-05-23 18:47:16 +0900

    [test] : `feedback 생성` 인수테스트 작성 (#85)

---
+++ api/acceptance-test/src/test/java/me/nalab/luffy/api/acceptance/test/feedback/create/response/ShortFormQuestionResponse.java
@@ -0,0 +1,16 @@
+package me.nalab.luffy.api.acceptance.test.feedback.create.response;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@SuperBuilder
+@Getter
+@ToString
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+public class ShortFormQuestionResponse extends FormQuestionResponseable {
+
+}
