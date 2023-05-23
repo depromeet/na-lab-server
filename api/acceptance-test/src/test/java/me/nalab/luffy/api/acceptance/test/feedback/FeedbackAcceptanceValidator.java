c20b1c345b5e23fb52f749f15fe3bfc92c8e99bf
xb205 <62425964+devxb@users.noreply.github.com>
2023-05-23 15:24:54 +0900

    [test] : feedback `인수테스트 supporter` 추가 (#82)

---
+++ api/acceptance-test/src/test/java/me/nalab/luffy/api/acceptance/test/feedback/FeedbackAcceptanceValidator.java
@@ -0,0 +1,11 @@
+package me.nalab.luffy.api.acceptance.test.feedback;
+
+public final class FeedbackAcceptanceValidator {
+
+	private FeedbackAcceptanceValidator() {
+		throw new UnsupportedOperationException("Cannot invoke constructor \"FeedbackAcceptanceValidator()\"");
+	}
+
+	//:TODO 여기에 Feedback 인수테스트 validator 로직을 static으로 작성 해주세요. SurveyAcceptanceValidator 참고해서 작성하면 됩니다.
+
+}
