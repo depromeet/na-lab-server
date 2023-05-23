974c4dda7e4d23c3b50c61fe630a8b93a69a2078
수지니 <71487608+ssssujini99@users.noreply.github.com>
2023-05-23 18:54:14 +0900

    [test] : 질문 폼의 피드백 요약 인수테스트 작성

--- api/acceptance-test/src/test/java/me/nalab/luffy/api/acceptance/test/feedback/FeedbackAcceptanceValidator.java
+++ api/acceptance-test/src/test/java/me/nalab/luffy/api/acceptance/test/feedback/FeedbackAcceptanceValidator.java
@@ -1,5 +1,7 @@
 package me.nalab.luffy.api.acceptance.test.feedback;
 
+import org.springframework.test.web.servlet.ResultActions;
+
 public final class FeedbackAcceptanceValidator {
 
 	private FeedbackAcceptanceValidator() {
@@ -8,4 +10,13 @@ public final class FeedbackAcceptanceValidator {
 
 	//:TODO 여기에 Feedback 인수테스트 validator 로직을 static으로 작성 해주세요. SurveyAcceptanceValidator 참고해서 작성하면 됩니다.
 
+	//: TODO
+	public static void assertIsFeedbackSummaryFound(ResultActions resultActions) throws Exception {
+		resultActions.andExpectAll(
+			status().isOk(),
+			content().contentType(MediaType.APPLICATION_JSON),
+			jsonPath("$.all_feedback_count").isNumber(),
+			jsonPath("$.new_feedback_count").isNumber()
+		);
+	}
 }
