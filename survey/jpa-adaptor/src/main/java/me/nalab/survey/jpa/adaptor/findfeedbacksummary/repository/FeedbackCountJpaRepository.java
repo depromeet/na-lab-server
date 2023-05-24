fd7a7ec9570d93cf9729a105d964789698d41baa
수지니 <71487608+ssssujini99@users.noreply.github.com>
2023-05-25 08:52:47 +0900

    [fix] : getResultList 에서 countBy 로 변경

--- survey/jpa-adaptor/src/main/java/me/nalab/survey/jpa/adaptor/findfeedbacksummary/repository/FeedbackCountJpaRepository.java
+++ survey/jpa-adaptor/src/main/java/me/nalab/survey/jpa/adaptor/findfeedbacksummary/repository/FeedbackCountJpaRepository.java
@@ -1,15 +1,13 @@
 package me.nalab.survey.jpa.adaptor.findfeedbacksummary.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import me.nalab.core.data.feedback.FeedbackEntity;
 
 public interface FeedbackCountJpaRepository extends JpaRepository<FeedbackEntity, Long> {
 
-	List<FeedbackEntity> findBySurveyId(Long surveyId);
+	Long countBySurveyId(Long surveyId);
 
-	List<FeedbackEntity> findBySurveyIdAndIsRead(Long surveyId, boolean isRead);
+	Long countBySurveyIdAndIsReadFalse(Long surveyId);
 
 }
