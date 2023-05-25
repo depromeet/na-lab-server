81d116b5a5713050cc7b928f9ff3bc35569d113b
수지니 <71487608+ssssujini99@users.noreply.github.com>
2023-05-26 04:21:49 +0900

    [feat] : 질문 폼의 모든 리뷰어 조회 유즈케이스 구현체 구현

---
+++ survey/application/src/main/java/me/nalab/survey/application/service/feedbackreviewers/FeedbackReviewersFindService.java
@@ -0,0 +1,22 @@
+package me.nalab.survey.application.service.feedbackreviewers;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import me.nalab.survey.application.port.in.web.feedbackreviewers.FeedbackReviewersFindUseCase;
+import me.nalab.survey.application.port.out.persistence.feedbackreviewers.FeedbacksFindPort;
+import me.nalab.survey.domain.feedback.Feedback;
+
+@Service
+@RequiredArgsConstructor
+public class FeedbackReviewersFindService implements FeedbackReviewersFindUseCase {
+
+	private final FeedbacksFindPort feedbacksFindPort;
+
+	@Override
+	public List<Feedback> findAllFeedback(Long surveyId) {
+		return feedbacksFindPort.findAllFeedback(surveyId);
+	}
+}
