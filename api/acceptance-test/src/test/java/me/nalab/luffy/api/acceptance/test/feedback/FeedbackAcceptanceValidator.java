1b9d7391956381437d3aca2d9ffaa2644a708845
수지니 <71487608+ssssujini99@users.noreply.github.com>
2023-05-25 18:28:57 +0900

    [test] : 질문 폼의 요약정보 조회 인수테스트 작성

--- api/acceptance-test/src/test/java/me/nalab/luffy/api/acceptance/test/feedback/FeedbackAcceptanceValidator.java
+++ api/acceptance-test/src/test/java/me/nalab/luffy/api/acceptance/test/feedback/FeedbackAcceptanceValidator.java
@@ -1,7 +1,10 @@
 package me.nalab.luffy.api.acceptance.test.feedback;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.ResultActions;
 
 public final class FeedbackAcceptanceValidator {
@@ -12,6 +15,15 @@ public final class FeedbackAcceptanceValidator {
 
 	public static void assertIsFeedbackCreated(ResultActions resultActions) throws Exception {
 		resultActions.andExpectAll(status().isCreated());
+	}
+
+	public static void assertIsFeedbackSummaryFound(ResultActions resultActions) throws Exception {
+		resultActions.andExpectAll(
+			status().isOk(),
+			content().contentType(MediaType.APPLICATION_JSON),
+			jsonPath("$.all_feedback_count").isNumber(),
+			jsonPath("$.new_feedback_count").isNumber()
+		);
 	}
 
 }
