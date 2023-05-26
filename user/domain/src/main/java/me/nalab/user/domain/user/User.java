f1859b16ea8abd484beec9fdf8f52148566646c3
유도진 <djyou128@gmail.com>
2023-05-26 22:06:15 +0900

    [domain] User 도메인 정의 (#128)

---
+++ user/domain/src/main/java/me/nalab/user/domain/user/User.java
@@ -0,0 +1,20 @@
+package me.nalab.user.domain.user;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@Builder
+@Getter
+@EqualsAndHashCode(of = {"id"})
+@AllArgsConstructor
+public class User {
+	private final long id;
+	private final Provider provider;
+	private final String token;
+	private final LocalDateTime createdAt;
+	private final LocalDateTime updatedAt;
+}
