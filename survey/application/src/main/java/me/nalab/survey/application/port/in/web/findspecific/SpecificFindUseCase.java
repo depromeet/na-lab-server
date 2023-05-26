a1d07dd34eeedf1c220b2d9f676d840bb5c9e4a8
수지니 <71487608+ssssujini99@users.noreply.github.com>
2023-05-27 00:10:17 +0900

    [feat] : 개별 피드백 응답 조회 - `Application` 구현 완료 (#131)

---
+++ survey/application/src/main/java/me/nalab/survey/application/port/in/web/findspecific/SpecificFindUseCase.java
@@ -0,0 +1,26 @@
+package me.nalab.survey.application.port.in.web.findspecific;
+
+import me.nalab.survey.application.common.feedback.dto.FeedbackDto;
+import me.nalab.survey.application.common.survey.dto.SurveyDto;
+
+/**
+ * 개별 Feedback의 구체 정보를 반환하는 UseCase 입니다.
+ */
+public interface SpecificFindUseCase {
+
+	/**
+	 * feedbackId를 입력으로 받아, feedbackId에 해당하는 FeedbackDto를 반환합니다.
+	 *
+	 * @param feedbackId Feedback의 id
+	 * @return FeedbackDto, Feedback을 FeedbackDto로 변환한 Dto type 을 반환합니다.
+	 */
+	FeedbackDto findFeedbackByFeedbackId(Long feedbackId);
+
+	/**
+	 * surveyId를 입력으로 받아, surveyId에 해당하는 SurveyDto를 반환합니다.
+	 *
+	 * @param surveyId survey의 id
+	 * @return SurveyDto, Survey를 SurveyDto로 변환한 Dto type 을 반환합니다.
+	 */
+	SurveyDto findSurveyBySurveyId(Long surveyId);
+}
---
+++ survey/application/src/main/java/me/nalab/survey/application/service/findspecific/SpecificFindService.java
@@ -0,0 +1,44 @@
+package me.nalab.survey.application.service.findspecific;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import me.nalab.survey.application.common.feedback.dto.FeedbackDto;
+import me.nalab.survey.application.common.feedback.mapper.FeedbackDtoMapper;
+import me.nalab.survey.application.common.survey.dto.SurveyDto;
+import me.nalab.survey.application.common.survey.mapper.SurveyDtoMapper;
+import me.nalab.survey.application.exception.FeedbackDoesNotExistException;
+import me.nalab.survey.application.exception.SurveyDoesNotExistException;
+import me.nalab.survey.application.exception.SurveyDoesNotHasTargetException;
+import me.nalab.survey.application.port.in.web.findspecific.SpecificFindUseCase;
+import me.nalab.survey.application.port.out.persistence.findspecific.FeedbackFindPort;
+import me.nalab.survey.application.port.out.persistence.findspecific.SurveyFindPort;
+import me.nalab.survey.domain.feedback.Feedback;
+import me.nalab.survey.domain.survey.Survey;
+
+@Service
+@RequiredArgsConstructor
+public class SpecificFindService implements SpecificFindUseCase {
+
+	private final FeedbackFindPort feedbackFindPort;
+	private final SurveyFindPort surveyFindPort;
+
+	@Override
+	public FeedbackDto findFeedbackByFeedbackId(Long feedbackId) {
+		Feedback feedback = feedbackFindPort.findFeedback(feedbackId).orElseThrow(() -> {
+			throw new FeedbackDoesNotExistException(feedbackId);
+		});
+		return FeedbackDtoMapper.toDto(feedback);
+	}
+
+	@Override
+	public SurveyDto findSurveyBySurveyId(Long surveyId) {
+		Survey survey = surveyFindPort.findSurvey(surveyId).orElseThrow(() -> {
+			throw new SurveyDoesNotExistException(surveyId);
+		});
+		Long targetId = surveyFindPort.findTargetIdBySurveyId(surveyId).orElseThrow(() -> {
+			throw new SurveyDoesNotHasTargetException(surveyId);
+		});
+		return SurveyDtoMapper.toSurveyDto(targetId, survey);
+	}
+}
