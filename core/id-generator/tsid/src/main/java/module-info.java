96bb9c64a3f3b55a9d3f418b97d72f5988b8d6cf
xb205 <62425964+devxb@users.noreply.github.com>
2023-05-26 17:55:58 +0900

    [feat] : TSID id generator 추가 (#125)

---
+++ core/id-generator/tsid/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module luffy.core.id.generator.tsid.main {
+
+	exports me.nalab.core.idgenerator.tsid.engine;
+	exports me.nalab.core.idgenerator.tsid;
+
+	requires luffy.core.id.generator.id.core.main;
+	requires com.github.f4b6a3.tsid;
+
+}
