d56acd84949b2637c2ec14028185f4c8d2421f66
devxb <dlwnsdud205@naver.com>
2023-05-26 23:53:46 +0900

    [chore] : api 에서 다른모듈로 의존성 추가

--- api/src/main/java/me/nalab/api/NalabApplication.java
+++ api/src/main/java/me/nalab/api/NalabApplication.java
@@ -2,8 +2,14 @@ package me.nalab.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan("me.nalab")
+@EnableJpaRepositories(basePackages = {"me.nalab"})
+@EntityScan(basePackages = {"me.nalab"})
 public class NalabApplication {
 
 	public static void main(String... args){
