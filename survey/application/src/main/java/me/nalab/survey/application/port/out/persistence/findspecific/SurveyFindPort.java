a67cec147fc3d5a8a81c634170dafab2a3623b08
수지니 <71487608+ssssujini99@users.noreply.github.com>
2023-05-26 23:47:57 +0900

    [feat] : 포트 interface 추가

---
+++ survey/application/src/main/java/me/nalab/survey/application/port/out/persistence/findspecific/SurveyFindPort.java
@@ -0,0 +1,27 @@
+package me.nalab.survey.application.port.out.persistence.findspecific;
+
+import java.util.Optional;
+
+import me.nalab.survey.domain.survey.Survey;
+
+/**
+ * surveyId를 받아 Survey를 찾는 역할을 하는 인터페이스 입니다.
+ */
+public interface SurveyFindPort {
+
+	/**
+	 * surveyId에 해당하는 Survey를 조회합니다.
+	 *
+	 * @param surveyId survey를 조회할 Survey의 ID
+	 * @return Optional 만약, 어떠한 surveyId도 없을 경우, Optional.empty() 를 반환해야 합니다.
+	 */
+	Optional<Survey> findSurvey(Long surveyId);
+
+	/**
+	 * surveyId에 해당하는 Target의 id를 조회합니다.
+	 *
+	 * @param surveyId survey의 ID
+	 * @return Optional 만약, 어떠한 targetId도 없을 경우, Optional.empty() 를 반환해야 합니다.
+	 */
+	Optional<Long> findTargetIdBySurveyId(Long surveyId);
+}
