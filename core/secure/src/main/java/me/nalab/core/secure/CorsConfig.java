8d4a0db589f437f00383f3044cf73d6723a27374
devxb <dlwnsdud205@naver.com>
2023-06-03 14:31:34 +0900

    [feat] : CORS 에러 핸들러 추가

---
+++ core/secure/src/main/java/me/nalab/core/secure/CorsConfig.java
@@ -0,0 +1,18 @@
+package me.nalab.core.secure;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+class CorsConfig implements WebMvcConfigurer {
+	private static final String[] ALLOWED_METHOD_NAMES = new String[]{"GET","HEAD","POST","PUT","DELETE","TRACE","OPTIONS","PATCH"};
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/v1/**")
+			.allowedMethods(ALLOWED_METHOD_NAMES)
+			.exposedHeaders(HttpHeaders.LOCATION);
+	}
+}
---
+++ core/secure/src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module luffy.core.secure.main {
+	requires spring.webmvc;
+	requires spring.web;
+	requires spring.context;
+}
