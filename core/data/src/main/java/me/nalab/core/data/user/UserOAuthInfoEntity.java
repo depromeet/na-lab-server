85b781dfb34a6e08c4bbcc0f57aaf144c17d216f
dojinyou <djyou128@gmail.com>
2023-06-05 10:57:39 +0900

    UserOAuthInfo 정의

--- core/data/src/main/java/me/nalab/core/data/user/UserOAuthInfoEntity.java
+++ core/data/src/main/java/me/nalab/core/data/user/UserOAuthInfoEntity.java
@@ -3,27 +3,39 @@ package me.nalab.core.data.user;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import me.nalab.core.data.common.TimeBaseEntity;
 
-@Table(name = "users")
+@Table(name = "user_oauth_info)")
 @Entity
 @Getter
 @SuperBuilder
-@NoArgsConstructor
 @AllArgsConstructor
-public class UserEntity extends TimeBaseEntity {
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserOAuthInfoEntity extends TimeBaseEntity {
 	@Id
 	private Long id;
 
-	@Column(nullable = false)
-	private String nickname;
+	@Column(nullable = false, updatable = false)
+	private String provider;
 
 	@Column(nullable = false)
 	private String email;
+
+	@Column(nullable = true)
+	private String username;
+
+	@Column(nullable = true)
+	private String phoneNumber;
+
+	@ManyToOne
+	private UserEntity userEntity;
 }
+
