ead533e731a0e427aca078e19f2a3d836b3f2a5e
devxb <dlwnsdud205@naver.com>
2023-06-07 04:13:43 +0900

    [feat] : Login된 유저 조회 컨트롤러 추가

---
+++ user/user-web-adaptor/src/main/java/me/nalab/user/web/adaptor/logined/LoginedUserGetController.java
@@ -0,0 +1,28 @@
+package me.nalab.user.web.adaptor.logined;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import me.nalab.user.application.common.dto.LoginedInfo;
+import me.nalab.user.application.port.in.LoginedUserGetByTokenUseCase;
+import me.nalab.user.web.adaptor.logined.response.LoginedInfoResponse;
+
+@RestController
+@RequestMapping("/v1")
+@RequiredArgsConstructor
+public class LoginedUserGetController {
+
+	private final LoginedUserGetByTokenUseCase loginedUserGetByTokenUseCase;
+
+	@GetMapping("/users")
+	public LoginedInfoResponse getLoginedUserByToken(@RequestHeader(HttpHeaders.AUTHORIZATION) String token) {
+		LoginedInfo loginedInfo = loginedUserGetByTokenUseCase.decryptToken(token);
+
+		return new LoginedInfoResponse(loginedInfo.getTargetId(), loginedInfo.getNickName());
+	}
+
+}
---
+++ user/user-web-adaptor/src/main/java/me/nalab/user/web/adaptor/logined/response/LoginedInfoResponse.java
@@ -0,0 +1,15 @@
+package me.nalab.user.web.adaptor.logined.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+
+@Data
+public class LoginedInfoResponse {
+
+	@JsonProperty("target_id")
+	private final long targetId;
+	@JsonProperty("nickname")
+	private final String nickName;
+
+}
