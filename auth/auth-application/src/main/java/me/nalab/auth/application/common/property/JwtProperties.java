67b37898a8f585efd07c7f5b4a0b2f20b4c81277
devxb <dlwnsdud205@naver.com>
2023-06-08 00:08:58 +0900

    [fix] : application.yml 못 읽어오는 버그 수정

--- auth/auth-application/src/main/java/me/nalab/auth/application/common/property/JwtProperties.java
+++ auth/auth-application/src/main/java/me/nalab/auth/application/common/property/JwtProperties.java
@@ -1,15 +1,33 @@
 package me.nalab.auth.application.common.property;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
 
-import lombok.Data;
+import lombok.Getter;
+import me.nalab.auth.application.config.YamlPropertySourceFactory;
 
-@Data
-@ConfigurationProperties(prefix = "jwt")
-public final class JwtProperties {
+@Getter
+@Component
+@PropertySource(value = "classpath:application.yml", factory = YamlPropertySourceFactory.class)
+public class JwtProperties {
+
 	private final String accessTokenHeader;
 	private final String issuer;
 	private final String prefix;
 	private final String secret;
 	private final Long accessTokenExpirySeconds;
+
+	public JwtProperties(@Value("${jwt.access-token-header}") String accessTokenHeader,
+		@Value("${jwt.issuer}") String issuer,
+		@Value("${jwt.prefix}") String prefix,
+		@Value("${jwt.secret}") String secret,
+		@Value("${jwt.accessTokenExpirySeconds}") Long accessTokenExpirySeconds) {
+		this.accessTokenHeader = accessTokenHeader;
+		this.issuer = issuer;
+		this.prefix = prefix;
+		this.secret = secret;
+		this.accessTokenExpirySeconds = accessTokenExpirySeconds;
+	}
+
 }
