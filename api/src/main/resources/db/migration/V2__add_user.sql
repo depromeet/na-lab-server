7ff4c24261e535f10074674d2e4224cae750de80
devxb <dlwnsdud205@naver.com>
2023-06-10 15:22:21 +0900

    [fix] : user ddl 수정

--- api/src/main/resources/db/migration/V2__add_user.sql
+++ api/src/main/resources/db/migration/V2__add_user.sql
@@ -1,5 +1,7 @@
-create table user (
+create table users (
     id bigint not null,
+    created_at timestamp(6) not null,
+    updated_at timestamp(6) not null,
     nickname varchar(255) not null,
     email varchar(255) not null,
     primary key (id)
@@ -12,8 +14,8 @@ create table user_oauth_info (
     username varchar(255),
     phone_number varchar(255),
     user_entity_id bigint,
-    created_at timestamp,
-    updated_at timestamp,
+    created_at timestamp(6),
+    updated_at timestamp(6),
     primary key (id),
-    foreign key (user_entity_id) references user_entity (id)
+    foreign key (user_entity_id) references users (id)
 );
