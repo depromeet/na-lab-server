bad1ce762a2a77a98d7d0c07b3e2dec6c4f29d40
수지니 <71487608+ssssujini99@users.noreply.github.com>
2023-06-11 21:10:39 +0900

    [Refactor] : 코드 리팩토링

--- survey/application/src/main/java/me/nalab/survey/application/service/findspecific/SpecificFindService.java
+++ survey/application/src/main/java/me/nalab/survey/application/service/findspecific/SpecificFindService.java
@@ -36,9 +36,9 @@ public class SpecificFindService implements SpecificFindUseCase {
 
 	@Override
 	public void updateFeedbackIsReadByFeedbackId(Long feedbackId) {
-		feedbackUpdatePort.updateFeedbackIsReadByFeedbackId(feedbackId).orElseThrow(() -> {
+		if (feedbackUpdatePort.updateFeedbackIsReadByFeedbackId(feedbackId).isEmpty()) {
 			throw new FeedbackDoesNotExistException(feedbackId);
-		});
+		}
 	}
 
 	@Override
