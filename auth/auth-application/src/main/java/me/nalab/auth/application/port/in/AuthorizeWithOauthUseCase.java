49c4f2fc98cecd6949b5ddd20a9ecca6d7304650
dojinyou <djyou128@gmail.com>
2023-06-13 07:00:49 +0900

    usecase 주석 추가

--- auth/auth-application/src/main/java/me/nalab/auth/application/port/in/AuthorizeWithOauthUseCase.java
+++ auth/auth-application/src/main/java/me/nalab/auth/application/port/in/AuthorizeWithOauthUseCase.java
@@ -2,6 +2,15 @@ package me.nalab.auth.application.port.in;
 
 import java.net.URI;
 
+/**
+ * OAuth Provider의 인증 허가(authorize) 처리합니다
+ */
 public interface AuthorizeWithOauthUseCase {
+
+    /**
+     * 인증 허가를 위한 uri을 가져옵니다
+     * @param providerName provider 이름
+     * @return 인증 허가 uri
+     */
     URI getAuthorizationUrl(String providerName);
 }
