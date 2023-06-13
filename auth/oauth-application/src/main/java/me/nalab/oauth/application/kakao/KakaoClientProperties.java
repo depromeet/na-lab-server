767a45f01e6a1c4a996ad2ab1632c6a5962795e6
유도진 <djyou128@gmail.com>
2023-06-14 00:48:47 +0900

    [feat] authorize를 위한 redirect 구현 (#213)

--- auth/oauth-application/src/main/java/me/nalab/oauth/application/kakao/KakaoClientProperties.java
+++ auth/oauth-application/src/main/java/me/nalab/oauth/application/kakao/KakaoClientProperties.java
@@ -2,40 +2,45 @@ package me.nalab.oauth.application.kakao;
 
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.stereotype.Component;
 
 
 @Getter
-@Component
+@Configuration
 @PropertySource("classpath:application.properties")
 public class KakaoClientProperties {
 
     private final String baseUrl;
-    private final String accessTokenUrl;
-    private final String redirectUrl;
+    private final String authorizePath;
+    private final String accessTokenPath;
+    private final String redirectPath;
     private final String clientId;
     private final String secret;
     private final String grantType;
+    private final String scope;
 
     public KakaoClientProperties(
             @Value("${oauth.kakao.base-url:https://kauth.kakao.com}") String baseUrl,
-            @Value("${oauth.kakao.access-token-url:/oauth/token}") String accessTokenUrl,
-            @Value("${oauth.kakao.redirect-url:/oauth/token}") String redirectUrl,
-            @Value("${oauth.kakao.clientId:clientId}") String clientId,
+            @Value("${oauth.kakao.authorize-path:/oauth/authorize}") String authorizePath,
+            @Value("${oauth.kakao.access-token-path:/oauth/token}") String accessTokenPath,
+            @Value("${oauth.kakao.redirect-path:/oauth/token}") String redirectPath,
+            @Value("${oauth.kakao.client-id:clientId}") String clientId,
             @Value("${oauth.kakao.secret:secret}") String secret,
-            @Value("${oauth.kakao.grant-type:authorization_code}") String grantType
-    ) {
+            @Value("${oauth.kakao.grant-type:authorization_code}") String grantType,
+            @Value("${oauth.kakao.scope:profile_nickname,account_email}") String scope) {
         this.baseUrl = baseUrl;
-        this.accessTokenUrl = accessTokenUrl;
-        this.redirectUrl = redirectUrl;
+        this.authorizePath = authorizePath;
+        this.accessTokenPath = accessTokenPath;
+        this.redirectPath = redirectPath;
         this.clientId = clientId;
         this.secret = secret;
         this.grantType = grantType;
+        this.scope = scope;
     }
 
     public String getAccessUrl() {
-        return baseUrl + accessTokenUrl;
+        return baseUrl + accessTokenPath;
     }
 }
 
