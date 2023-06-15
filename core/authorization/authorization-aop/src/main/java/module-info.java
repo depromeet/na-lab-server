51c5f5265aaf265e1f8dc0e2ea46ac700653f455
devxb <dlwnsdud205@naver.com>
2023-06-15 17:21:41 +0900

    [chore] : module-info 추가

---
+++ core/authorization/authorization-aop/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module luffy.core.authorization.authorization.aop.main {
+
+	requires org.apache.tomcat.embed.core;
+	requires org.aspectj.weaver;
+	requires spring.context;
+	requires lombok;
+	requires luffy.core.authorization.authorization.core.main;
+
+}
