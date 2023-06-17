8c32483b01a21f3d3712b3db338b63e29ea7c832
devxb <dlwnsdud205@naver.com>
2023-06-17 13:57:54 +0900

    [chore] : 주석 삭제

--- core/authorization/authorization-aop/src/main/java/module-info.java
+++ core/authorization/authorization-aop/src/main/java/module-info.java
@@ -1,11 +1,13 @@
-// module luffy.core.authorization.authorization.aop.main {
-//
-// 	exports me.nalab.core.authorization.aop.meta;
-//
-// 	requires org.apache.tomcat.embed.core;
-// 	requires org.aspectj.weaver;
-// 	requires spring.context;
-// 	requires lombok;
-// 	requires luffy.core.authorization.authorization.core.main;
-//
-// }
+module luffy.core.authorization.authorization.aop.main {
+
+	exports me.nalab.core.authorization.aop.meta;
+
+	requires org.apache.tomcat.embed.core;
+	requires org.aspectj.weaver;
+	requires spring.context;
+	requires lombok;
+	requires luffy.core.authorization.authorization.core.main;
+	requires spring.web;
+	requires com.fasterxml.jackson.annotation;
+
+}
