e5835e50b3885313ff914beb86a5f6a0a43f930a
dojinyou <djyou128@gmail.com>
2023-06-17 17:30:00 +0900

    logback, sentry 설정

--- core/exception-handler/src/main/java/me/nalab/core/exception/handler/ValidationControllerAdvice.java
+++ core/exception-handler/src/main/java/me/nalab/core/exception/handler/ValidationControllerAdvice.java
@@ -1,5 +1,6 @@
 package me.nalab.core.exception.handler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 /**
  * javax.validation 어노테이션의 message를 잡아 내려줍니다
  */
+@Slf4j
 @RestControllerAdvice
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class ValidationControllerAdvice {
@@ -18,6 +20,8 @@ public class ValidationControllerAdvice {
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	public ErrorTemplate catchValidationException(MethodArgumentNotValidException methodArgumentNotValidException) {
+		log.error(methodArgumentNotValidException.getMessage(), methodArgumentNotValidException);
+
 		String messages = methodArgumentNotValidException
 			.getBindingResult()
 			.getAllErrors()
