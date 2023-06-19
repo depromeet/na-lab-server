6ee1129d6a9acb4db0ef38f87afee486d8b66cac
dojinyou <djyou128@gmail.com>
2023-06-19 12:24:47 +0900

    revert의 revert

--- core/authorization/authorization-aop/src/main/java/me/nalab/core/authorization/aop/advice/AuthorizationExceptionAdvice.java
+++ core/authorization/authorization-aop/src/main/java/me/nalab/core/authorization/aop/advice/AuthorizationExceptionAdvice.java
@@ -1,5 +1,6 @@
 package me.nalab.core.authorization.aop.advice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -7,12 +8,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import me.nalab.core.authorization.api.CannotAuthorizationException;
 
+@Slf4j
 @RestControllerAdvice
 public class AuthorizationExceptionAdvice {
 
 	@ExceptionHandler(CannotAuthorizationException.class)
 	@ResponseStatus(HttpStatus.UNAUTHORIZED)
 	ErrorTemplate handleCannotAuthorizationException(CannotAuthorizationException exception) {
+		log.error(exception.getMessage(), exception);
 		return ErrorTemplate.of("Invalid token cause \"" + exception.getMessage() + "\"");
 	}
 
