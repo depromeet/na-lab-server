1c4c1424d76ec1d5ed2eb4919919cbecb741ade4
dojinyou <djyou128@gmail.com>
2023-06-19 17:51:43 +0900

    dsn debug 추가

--- api/src/main/java/me/nalab/api/sentry/SentryController.java
+++ api/src/main/java/me/nalab/api/sentry/SentryController.java
@@ -1,6 +1,7 @@
 package me.nalab.api.sentry;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,22 +11,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1/sentry")
 public class SentryController {
 
+    private final String dsn;
+
+    public SentryController(@Value("sentry.dsn") String dsn) {
+        this.dsn = dsn;
+    }
+
     @GetMapping("/info")
     public String info() {
         log.info("[info]sentry check");
-        return "info";
+        return "info - dsn:"+dsn;
     }
 
     @GetMapping("/warn")
     public String warn() {
         log.warn("[warn]sentry check");
-        return "warn";
+        return "warn - dsn:"+dsn;
     }
 
     @GetMapping("/error")
     public String error() {
         log.error("[error]sentry check");
-        return "error";
+        return "error - dsn:"+dsn;
     }
 
 }
