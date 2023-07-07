5a834a388e1396b5db14b10836cbd0dc020ec448
수지니 <71487608+ssssujini99@users.noreply.github.com>
2023-07-07 16:27:34 +0900

    [refactor] : 질문폼의 모든 피드백 조회 v2로 업데이트, is_read 버그 수정 (#302)

--- survey/survey-web-adaptor/src/main/java/me/nalab/survey/web/adaptor/findfeedback/response/feedback/AbstractFeedbackResponse.java
+++ survey/survey-web-adaptor/src/main/java/me/nalab/survey/web/adaptor/findfeedback/response/feedback/AbstractFeedbackResponse.java
@@ -15,11 +15,15 @@ public abstract class AbstractFeedbackResponse {
 
 	@JsonProperty("feedback_id")
 	private final String id;
+	@JsonProperty("form_question_feedback_id")
+	private final String formQuestionFeedbackId;
 	@JsonProperty("created_at")
 	private final LocalDateTime createdAt;
 	@JsonProperty("is_read")
 	private final boolean read;
 	@JsonProperty("reviewer")
 	private final ReviewerResponse reviewerResponse;
+	@JsonProperty("bookmark")
+	private final BookmarkResponse bookmarkResponse;
 
 }
