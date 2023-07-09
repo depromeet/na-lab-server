76f0f4ce2235e0bdb86b440bf44b95c44e50acb3
수지니 <71487608+ssssujini99@users.noreply.github.com>
2023-07-09 23:20:16 +0900

    [feat] : 질문폼의 북마크된 피드백 교체 - `web-adaptor` 구현 완료 (#312)
    Co-authored-by: devxb <dlwnsdud205@naver.com>
    Co-authored-by: xb205 <62425964+devxb@users.noreply.github.com>

--- survey/survey-domain/src/test/java/me/nalab/survey/domain/feedback/BookmarkTest.java
+++ survey/survey-domain/src/test/java/me/nalab/survey/domain/feedback/BookmarkTest.java
@@ -4,14 +4,36 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.Instant;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class BookmarkTest {
 
 	@Test
-	void bookmark_replacedIsBookmarked_test() {
+	@DisplayName("replaceIsBookmarked()메소드는 bookmark가 false라면, true로 변경한다.")
+	void bookmark_replacedIsBookmarked_true_if_false_test() {
 
 		boolean initialIsBookmarked = false;
+		Instant initialBookmarkedAt = Instant.now();
+
+		Bookmark bookmark = Bookmark.builder()
+			.isBookmarked(initialIsBookmarked)
+			.bookmarkedAt(initialBookmarkedAt)
+			.build();
+		bookmark.replaceIsBookmarked();
+
+		boolean updatedIsBookmarked = bookmark.isBookmarked();
+		Instant updatedBookmarkedAt = bookmark.getBookmarkedAt();
+
+		assertEquals(updatedIsBookmarked, !initialIsBookmarked);
+		assertNotEquals(updatedBookmarkedAt, initialBookmarkedAt);
+	}
+
+	@Test
+	@DisplayName("replaceIsBookmarked()메소드는 bookmark가 true라면, false로 변경한다.")
+	void bookmark_replacedIsBookmarked_false_if_true_test() {
+
+		boolean initialIsBookmarked = true;
 		Instant initialBookmarkedAt = Instant.now();
 
 		Bookmark bookmark = Bookmark.builder()
