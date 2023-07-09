c520cc5ad04ba4c644f6a57bb670ecf977bd5ec7
수지니 <71487608+ssssujini99@users.noreply.github.com>
2023-07-09 18:53:06 +0900

    feat: jpa-adaptor 추가

---
+++ survey/survey-jpa-adaptor/src/main/java/me/nalab/survey/jpa/adaptor/bookmark/FormQuestionFeedbackUpdateAdaptor.java
@@ -0,0 +1,24 @@
+package me.nalab.survey.jpa.adaptor.bookmark;
+
+import org.springframework.stereotype.Repository;
+
+import lombok.RequiredArgsConstructor;
+import me.nalab.core.data.feedback.FormFeedbackEntity;
+import me.nalab.survey.application.port.out.persistence.bookmark.FormQuestionFeedbackUpdatePort;
+import me.nalab.survey.domain.feedback.FormQuestionFeedbackable;
+import me.nalab.survey.jpa.adaptor.bookmark.repository.FormQuestionFeedbackUpdateRepository;
+import me.nalab.survey.jpa.adaptor.common.mapper.FeedbackEntityMapper;
+
+@Repository
+@RequiredArgsConstructor
+public class FormQuestionFeedbackUpdateAdaptor implements FormQuestionFeedbackUpdatePort {
+
+	private final FormQuestionFeedbackUpdateRepository formQuestionFeedbackUpdateRepository;
+
+	@Override
+	public void updateFormQuestionFeedback(FormQuestionFeedbackable formQuestionFeedbackable) {
+		FormFeedbackEntity formFeedbackEntity = FeedbackEntityMapper.toFormFeedbackEntity(formQuestionFeedbackable);
+		formQuestionFeedbackUpdateRepository.save(formFeedbackEntity);
+	}
+
+}
