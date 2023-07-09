12061a6f2e5d5bdaa615c261dc72e29a80ce4a4f
devxb <dlwnsdud205@naver.com>
2023-07-09 23:16:19 +0900

    [feat] : BookmarkReplaceController에 Authorization 추가

--- survey/survey-web-adaptor/src/main/java/me/nalab/survey/web/adaptor/bookmark/BookmarkReplaceController.java
+++ survey/survey-web-adaptor/src/main/java/me/nalab/survey/web/adaptor/bookmark/BookmarkReplaceController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import me.nalab.core.authorization.aop.meta.Authorization;
 import me.nalab.survey.application.port.in.web.bookmark.BookmarkReplaceUseCase;
+import me.nalab.survey.application.service.authorization.FormQuestionFeedbackIdValidatorFactory;
 
 @RestController
 @RequestMapping("/v1")
@@ -17,6 +19,7 @@ public class BookmarkReplaceController {
 	private final BookmarkReplaceUseCase bookmarkReplaceUseCase;
 
 	@PatchMapping("/feedbacks/bookmarks")
+	@Authorization(factory = FormQuestionFeedbackIdValidatorFactory.class)
 	public ResponseEntity<Void> replaceBookmark(
 		@RequestParam("form-question-feedback-id") String formQuestionFeedbackId) {
 		bookmarkReplaceUseCase.replaceBookmark(Long.valueOf(formQuestionFeedbackId));
