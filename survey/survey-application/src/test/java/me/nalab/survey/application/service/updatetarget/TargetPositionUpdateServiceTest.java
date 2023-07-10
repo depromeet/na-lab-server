0adf237b8b78e57bdd22b27e4b7c91f720c644ae
수지니 <71487608+ssssujini99@users.noreply.github.com>
2023-07-10 19:24:13 +0900

    test: 서비스 테스트 추가

---
+++ survey/survey-application/src/test/java/me/nalab/survey/application/service/updatetarget/TargetPositionUpdateServiceTest.java
@@ -0,0 +1,72 @@
+package me.nalab.survey.application.service.updatetarget;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import me.nalab.survey.application.TestIdGenerator;
+import me.nalab.survey.application.exception.TargetDoesNotExistException;
+import me.nalab.survey.application.port.out.persistence.target.update.TargetFindPort;
+import me.nalab.survey.application.port.out.persistence.target.update.TargetPositionUpdatePort;
+import me.nalab.survey.domain.target.Target;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {TestIdGenerator.class})
+class TargetPositionUpdateServiceTest {
+
+	private TargetPositionUpdateService targetPositionUpdateService;
+
+	@Mock
+	private TargetFindPort targetFindPort;
+
+	@Mock
+	private TargetPositionUpdatePort targetPositionUpdatePort;
+
+	@BeforeEach
+	void setUp() {
+		MockitoAnnotations.openMocks(this);
+		targetPositionUpdateService = new TargetPositionUpdateService(targetFindPort, targetPositionUpdatePort);
+	}
+
+	@Test
+	void UPDATE_TARGET_POSITION_SERVICE_SUCCESS_TEST() {
+
+		Long targetId = 123L;
+		Target target = Target.builder()
+			.id(targetId)
+			.nickname("sujin")
+			.build();
+
+		when(targetFindPort.findTarget(targetId)).thenReturn(Optional.of(target));
+
+		assertDoesNotThrow(() -> targetPositionUpdateService.updateTargetPosition(targetId, target.getPosition()));
+
+	}
+
+	@Test
+	void UPDATE_TARGET_POSITION_SERVICE_FAIL_TEST() {
+
+		Long targetId = 123L;
+		Target target = Target.builder()
+			.id(targetId)
+			.nickname("sujin")
+			.build();
+
+		when(targetFindPort.findTarget(targetId)).thenReturn(Optional.empty());
+
+		assertThrows(TargetDoesNotExistException.class,
+			() -> targetPositionUpdateService.updateTargetPosition(targetId, target.getPosition())
+		);
+
+	}
+
+}
