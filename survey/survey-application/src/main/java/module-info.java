3a4815473819796fa47ca8d1a83ae9c40f55fba4
수지니 <71487608+ssssujini99@users.noreply.github.com>
2023-07-17 17:40:14 +0900

    [feat] : survey 존재 유무 확인 - `web-adaptor` 구현 완료 (#350)

--- survey/survey-application/src/main/java/module-info.java
+++ survey/survey-application/src/main/java/module-info.java
@@ -37,6 +37,7 @@ module luffy.survey.application.main {
 	exports me.nalab.survey.application.port.in.web.findfeedback.formtype;
 	exports me.nalab.survey.application.port.out.persistence.findfeedback.formtype;
 	exports me.nalab.survey.application.port.out.persistence.existsurvey;
+	exports me.nalab.survey.application.port.in.web.existsurvey;
 
 	requires luffy.survey.domain.main;
 	requires luffy.core.id.generator.id.generator.starter.main;
