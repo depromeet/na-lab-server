a567b4c316c17f75ba4c0b224d657eabbae78b28
수지니 <71487608+ssssujini99@users.noreply.github.com>
2023-07-17 18:02:18 +0900

    Revert "[feat] : survey 존재 유무 확인 - `jpa-adaptor` 구현 완료" (#348)

--- survey/survey-jpa-adaptor/src/test/java/me/nalab/survey/jpa/adaptor/existsurvey/TestTargetRepository.java
+++
@@ -1,9 +0,0 @@
-package me.nalab.survey.jpa.adaptor.existsurvey;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import me.nalab.core.data.target.TargetEntity;
-
-public interface TestTargetRepository extends JpaRepository<TargetEntity, Long> {
-
-}
