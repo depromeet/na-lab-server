8c15ada72dfc18e604ce3db78e741b2aa0b4bafa
devxb <dlwnsdud205@naver.com>
2024-02-20 00:00:32 +0900

    [refactor] : GalleryResponse를 변경된 도메인에 맞게 수정한다

--- gallery/src/main/kotlin/me/nalab/gallery/domain/response/GalleryResponse.kt
+++ gallery/src/main/kotlin/me/nalab/gallery/domain/response/GalleryResponse.kt
@@ -14,19 +14,27 @@ data class GalleryResponse(
         user = User(
             gallery.userId().toString(),
             gallery.userName(),
-            gallery.userPosition(),
+            gallery.userNickname(),
+            gallery.userImageUrl(),
         ),
         survey = Survey(
             gallery.surveyId().toString(),
             gallery.surveyFeedbackCount(),
-            gallery.surveyFeedbackCount(),
+            gallery.surveySaveCount(),
+            gallery.feedback(),
+            gallery.tendencies()
+                .asSequence()
+                .map { Survey.Tendency(it.name, it.tendencyCount) }
+                .toList(),
         ),
     )
 
     data class User(
         val id: String,
         val name: String,
-        val position: String,
+        val nickname: String,
+        @JsonProperty("image_url")
+        val imageUrl: String?,
     )
 
     data class Survey(
@@ -35,5 +43,13 @@ data class GalleryResponse(
         val feedbackCount: Int,
         @JsonProperty("save_count")
         val saveCount: Int,
-    )
+        val feedback: String,
+        val tendencies: List<Tendency>,
+    ) {
+
+        data class Tendency(
+            val name: String,
+            val count: Int,
+        )
+    }
 }
