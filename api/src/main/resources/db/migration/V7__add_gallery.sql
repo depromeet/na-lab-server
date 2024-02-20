5868456bd88c871352e4df3674b790f29ad0108c
devxb <dlwnsdud205@naver.com>
2024-02-20 16:26:38 +0900

    [fix] : gallery ddl의 position을 TEXT에서 VARCHAR(255) 로 변경한다

--- api/src/main/resources/db/migration/V7__add_gallery.sql
+++ api/src/main/resources/db/migration/V7__add_gallery.sql
@@ -3,7 +3,7 @@ create table if not exists gallery (
     target_id BIGINT unique not null,
     survey_id BIGINT unique not null,
     bookmarked_count INT not null,
-    position TEXT not null,
+    position VARCHAR(255) not null,
     created_at TIMESTAMP(6) not null,
     updated_at TIMESTAMP(6) not null
 );
