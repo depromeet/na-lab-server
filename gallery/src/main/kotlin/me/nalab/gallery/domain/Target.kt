a84c3319d4823bbfe22bce92695304e181719059
devxb <dlwnsdud205@naver.com>
2024-02-20 17:33:11 +0900

    [refactor] : position을 job으로 네이밍을 변경하고, enum Job을 생성한다

--- gallery/src/main/kotlin/me/nalab/gallery/domain/Target.kt
+++ gallery/src/main/kotlin/me/nalab/gallery/domain/Target.kt
@@ -2,12 +2,15 @@ package me.nalab.gallery.domain
 
 import javax.persistence.Column
 import javax.persistence.Embeddable
+import javax.persistence.EnumType
+import javax.persistence.Enumerated
 
 @Embeddable
 class Target(
     @Column(name = "target_id", unique = true, nullable = false, updatable = false)
     val targetId: Long,
 
-    @Column(name = "position", nullable = false)
-    val position: String,
+    @Enumerated(EnumType.STRING)
+    @Column(name = "job", nullable = false, columnDefinition = "TEXT not null")
+    val job: Job,
 )
