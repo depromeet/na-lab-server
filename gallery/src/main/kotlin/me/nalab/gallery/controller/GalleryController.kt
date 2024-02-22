e178e437599e458d836ad01212edaee22c40954b
devxb <dlwnsdud205@naver.com>
2024-02-22 19:38:45 +0900

    feat: /v1/gallerys/previews 를 인증 uri 목록에 추가한다

--- gallery/src/main/kotlin/me/nalab/gallery/controller/GalleryController.kt
+++ gallery/src/main/kotlin/me/nalab/gallery/controller/GalleryController.kt
@@ -2,9 +2,12 @@ package me.nalab.gallery.controller
 
 import me.nalab.gallery.app.GalleryPreviewApp
 import me.nalab.gallery.domain.response.GalleryPreviewDto
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestAttribute
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -14,6 +17,7 @@ class GalleryController(
 ) {
 
     @GetMapping("/previews")
+    @ResponseStatus(HttpStatus.OK)
     fun getGalleryPreview(@RequestAttribute("logined") targetId: Long): GalleryPreviewDto =
         galleryPreviewApp.getGalleryPreview(targetId)
 
