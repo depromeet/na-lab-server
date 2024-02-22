b7eaf13a7c5dc08ad162c71380575bf5f801699f
devxb <dlwnsdud205@naver.com>
2024-02-22 19:52:02 +0900

    refactor: remove unused import

--- gallery/src/main/kotlin/me/nalab/gallery/controller/GalleryController.kt
+++ gallery/src/main/kotlin/me/nalab/gallery/controller/GalleryController.kt
@@ -3,7 +3,6 @@ package me.nalab.gallery.controller
 import me.nalab.gallery.app.GalleryPreviewApp
 import me.nalab.gallery.domain.response.GalleryPreviewDto
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestAttribute
 import org.springframework.web.bind.annotation.RequestMapping
