2a4c17974e4ac718acf407eb21b3f96095128273
devxb <dlwnsdud205@naver.com>
2024-02-23 11:59:59 +0900

    perf: bookmarked_survey 테이블에 unique index를 추가한다

--- api/src/main/resources/db/migration/V8__add_job_bookmark_img_to_target.sql
+++ api/src/main/resources/db/migration/V8__add_job_bookmark_img_to_target.sql
@@ -6,5 +6,6 @@ create table if not exists bookmarked_survey (
   target_id BIGINT not null,
   bookmarked_survey_id BIGINT not null,
   foreign key (target_id) references target (target_id),
-  foreign key (bookmarked_survey_id) references survey (survey_id)
+  foreign key (bookmarked_survey_id) references survey (survey_id),
+  unique index target_id_bookmarked_survey_id_idx (target_id, bookmarked_survey_id)
 );
