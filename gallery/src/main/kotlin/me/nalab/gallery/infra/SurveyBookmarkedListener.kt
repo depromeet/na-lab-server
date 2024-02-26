19e720346ac2de0b8bdd02d3075ffbeee420c163
devxb <dlwnsdud205@naver.com>
2024-02-27 08:54:08 +0900

    refactor: SurveyBookmarkedListener에 jitter wait를 추가한다

--- gallery/src/main/kotlin/me/nalab/gallery/infra/SurveyBookmarkedListener.kt
+++ gallery/src/main/kotlin/me/nalab/gallery/infra/SurveyBookmarkedListener.kt
@@ -3,21 +3,32 @@ package me.nalab.gallery.infra
 import me.nalab.gallery.domain.GalleryService
 import me.nalab.survey.application.port.out.persistence.bookmark.SurveyBookmarkListenPort
 import org.springframework.dao.OptimisticLockingFailureException
+import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Service
+import kotlin.random.Random
 
 @Service
 class SurveyBookmarkedListener(
     private val galleryService: GalleryService,
-): SurveyBookmarkListenPort {
+) : SurveyBookmarkListenPort {
 
+    @Async
     override fun listenBookmarked(targetId: Long) {
         runCatching {
             galleryService.increaseBookmarkCount(targetId)
         }.recoverCatching {
-            when(it is OptimisticLockingFailureException) {
-                true -> listenBookmarked(targetId)
+            when (it is OptimisticLockingFailureException) {
+                true -> {
+                    waitJitter()
+                    listenBookmarked(targetId)
+                }
+
                 false -> throw it
             }
         }
+    }
+
+    private fun waitJitter() {
+        Thread.sleep(Random.nextLong(500, 1000))
     }
 }
