3200ead40c5a4791f28e76284f946ed0b2aa5417
devxb <dlwnsdud205@naver.com>
2024-03-06 20:01:16 +0900

    feat: DuplicateSurveyException 핸들러를 추가한다

--- survey/survey-web-adaptor/src/main/java/me/nalab/survey/web/adaptor/advice/SurveyControllerAdvice.java
+++ survey/survey-web-adaptor/src/main/java/me/nalab/survey/web/adaptor/advice/SurveyControllerAdvice.java
@@ -1,12 +1,12 @@
 package me.nalab.survey.web.adaptor.advice;
 
+import me.nalab.survey.application.exception.DuplicateSurveyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import me.nalab.survey.application.exception.SurveyDoesNotExistException;
-import me.nalab.survey.application.exception.SurveyDoesNotHasTargetException;
 import me.nalab.survey.application.exception.TargetDoesNotExistException;
 import me.nalab.survey.application.exception.TargetDoesNotHasSurveyException;
 
@@ -30,6 +30,12 @@ public class SurveyControllerAdvice {
 	ErrorTemplate handleSurveyDoesNotExistException(SurveyDoesNotExistException surveyDoesNotExistException) {
 		return ErrorTemplate.of(
 			"Cannot found any survey form id \"" + surveyDoesNotExistException.getSurveyId() + "\"");
+	}
+
+	@ExceptionHandler(DuplicateSurveyException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ErrorTemplate handleDuplicateSurveyException(DuplicateSurveyException duplicateSurveyException) {
+		return ErrorTemplate.of(duplicateSurveyException.getMessage());
 	}
 
 }
