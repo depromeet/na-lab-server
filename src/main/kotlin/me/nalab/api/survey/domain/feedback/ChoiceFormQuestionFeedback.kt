c9222faa7d42506a16b79e0645e74044fc4d5b61
devxb <dlwnsdud205@naver.com>
2024-03-10 10:17:16 +0900

    refactor: ChoiceFormQuestionFeedback 엔티티가 북마크를 매개변수로 받지않도록 수정한다

--- src/main/kotlin/me/nalab/api/survey/domain/feedback/ChoiceFormQuestionFeedback.kt
+++ src/main/kotlin/me/nalab/api/survey/domain/feedback/ChoiceFormQuestionFeedback.kt
@@ -7,11 +7,10 @@ class ChoiceFormQuestionFeedback(
     id: Long,
     formQuestionId: Long,
     isRead: Boolean = false,
-    bookmark: Bookmark = Bookmark.impossible(),
     feedback: Feedback,
 
     @ElementCollection
     @Column(name = "selects")
     @CollectionTable(name = "selects", joinColumns = [JoinColumn(name = "form_feedback_id")])
     private val selectedChoiceIds: MutableSet<Long>,
-) : FormQuestionFeedbackable(id, formQuestionId, isRead, bookmark, feedback)
+) : FormQuestionFeedbackable(id, formQuestionId, isRead, Bookmark.impossible(), feedback)
