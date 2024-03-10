9123154544d448ca4de1d9a02e1c083e4570de04
devxb <dlwnsdud205@naver.com>
2024-03-10 10:11:12 +0900

    refactor: TimeUtil을 object로 변경한다

--- src/main/kotlin/me/nalab/api/core/TimeUtil.kt
+++ src/main/kotlin/me/nalab/api/core/TimeUtil.kt
@@ -5,32 +5,30 @@ import java.time.Instant
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 
-class TimeUtil private constructor() {
+object TimeUtil {
 
-    companion object {
-        private var clock: Clock? = null
+    private var clock: Clock? = null
 
-        fun toInstant(): Instant {
-            var current = Instant.now()
-            if (clock != null) {
-                current = Instant.now(clock)
-            }
-            return formatTo6Digit(current)
+    fun toInstant(): Instant {
+        var current = Instant.now()
+        if (clock != null) {
+            current = Instant.now(clock)
         }
+        return formatTo6Digit(current)
+    }
 
-        private fun formatTo6Digit(instant: Instant): Instant {
-            val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSSX")
-                .withZone(ZoneId.of("UTC"))
-            return Instant.parse(formatter.format(instant))
-        }
+    private fun formatTo6Digit(instant: Instant): Instant {
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSSX")
+            .withZone(ZoneId.of("UTC"))
+        return Instant.parse(formatter.format(instant))
+    }
 
-        fun fixed(clock: Clock?) {
-            Companion.clock = clock
-        }
+    fun fixed(clock: Clock?) {
+        this.clock = clock
+    }
 
-        fun clear() {
-            clock = null
-        }
+    fun clear() {
+        clock = null
     }
 }
 
